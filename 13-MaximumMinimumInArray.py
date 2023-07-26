9bbe789c5bc5d3030a33dec879c495bfa79736f8
Gyanender Kumar <72467942+Gyani25k@users.noreply.github.com>
2023-07-26 17:22:47 +0530

    Add files via upload

---
+++ 13-MaximumMinimumInArray.py
@@ -0,0 +1,15 @@
+'''Given an array arr of size n, find maximum and minimum elements in the array.'''
+
+
+
+def maximumElement(arr,n,):
+    #code here
+    result=max(arr)
+    return result
+
+
+
+def minimumElement(arr,n):
+    #code here
+    result=min(arr)
+    return result
